b9f3ce1e76dc77e15222a3d2b96bcd44a8de76fc
Tihomir Dimitrov <dimitrov@leandigitalsolutions.com>
2019-01-15 11:26:02 +0200

    Initial Commit

---
+++ Todoey-2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey-2019
+//
+//  Created by Tihomir Dimitrov on 15.01.19.
+//  Copyright © 2019 Tihomir Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
